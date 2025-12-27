f803a37ad59c09e43a9992e19ca03e3015e7556c
Yassine L <yassinelutumba.98@gmail.com>
2025-12-28 05:00:52 +0700

    init

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-6c4!$e-91n&1yzay_jj&wim+27)hm+q8-2d!ha_ugosyrv8ldk"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
