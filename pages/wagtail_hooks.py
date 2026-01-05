4232bf6767865cd50ca644cd608bbc73f37b5277
Yassine L <yassinelutumba.98@gmail.com>
2026-01-05 17:10:08 +0700

    init

--- pages/wagtail_hooks.py
+++ pages/wagtail_hooks.py
@@ -5,9 +5,9 @@ from django.templatetags.static import static
 @hooks.register("insert_global_admin_js")
 def media_picker_admin_js():
 
-    return format_html('<script src="{}"></script>', static("js/media_position_picker.js"))
+    return format_html('<script src="{}"></script>', static("js/media-position-picker.js"))
 
 @hooks.register("insert_global_admin_css")
 def media_picker_admin_css():
-    return format_html('<link rel="stylesheet" href="{}">', static("css/media_position_picker.css"))
+    return format_html('<link rel="stylesheet" href="{}">', static("css/media-position-picker.css"))
 
